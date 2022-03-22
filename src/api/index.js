222bcf94ace0164cd7f4a19b223819ad1c9606ec
Cherryl - Kave Blocks <cherryl@kaveblocks.com>
2022-03-22 13:30:59 +0800

    finished the first project for travel advisor

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,41 @@
+/* eslint-disable consistent-return */
 import axios from 'axios';
 
-export const getPlacesData = async(type, sw, ne) => {
+export const getPlacesData = async (type, sw, ne) => {
   try {
     const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
       params: {
         bl_latitude: sw.lat,
-        tr_latitude: ne.lat,
         bl_longitude: sw.lng,
         tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
       },
       headers: {
+        'x-rapidapi-key': process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
         'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-        'x-rapidapi-key': 'bb64618b1bmsh112d868a0415326p1a507djsn847cc3352cec'
-      }
+      },
     });
 
     return data;
-
-
-  }catch(error){
-      console.log(error)
+  } catch (error) {
+    console.log(error);
   }
-}
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
+        params: { lat, lon: lng },
+        headers: {
+          'x-rapidapi-key': process.env.REACT_APP_RAPID_API_WEATHER_API_KEY,
+          'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+        },
+      });
+
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,46 +1,35 @@
 import React from 'react';
 import { Autocomplete } from '@react-google-maps/api';
-import { AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
-import useStyles from './styles';
+import useStyles from './styles.js';
 
-const Header = ({setCoordinates}) => {
+const Header = ({ onPlaceChanged, onLoad }) => {
   const classes = useStyles();
-  const [autocomplete, setAutoComplete] = useState(null);
-
-  const onLoad = (autoC) => setAutoComplete(autoC); 
-
-  const onPlaceChanged = () => {
-    const lat = autocomplete.getPlace().geometry.location.lat();
-    const lng = autocomplete.getPlace().geometry.location.lng();
-  
-    setCoordinates({lat, lng});
-  
-  }
 
   return (
-    <AppBar position='static'>
-      <Toolbar className= {classes.toolbar}>
-        <Typography variant = 'h5' className={classes.tittle}>
-          Travel Advisor  
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
         </Typography>
         <Box display="flex">
-          <Typography variant = 'h6' className={classes.tittle}>
+          <Typography variant="h6" className={classes.title}>
             Explore new places
           </Typography>
           <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
-            <div className={classes.search}> 
+            <div className={classes.search}>
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
-              <InputBase placeholder="Search ..." classes={{ root: classes.inputRoot, input: classes.inputInpit}}/>
+              <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
             </div>
           </Autocomplete>
         </Box>
       </Toolbar>
     </AppBar>
   );
-}
+};
 
 export default Header;
